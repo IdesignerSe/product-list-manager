f05377334426082cbc02752d8db607104ceefbb5
idesignerSe <hebera75@gmail.com>
2026-05-08 08:32:38 +0200

    Start Point 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("PRODUCT LIST MANAGER");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
--- obj/Debug/net10.0/ProductListManager.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductListManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductListManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bf7d9074c4ca25971cee855c079657409f54fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0811b79cee073b6ea86589895ff3914a075ab79d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductListManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductListManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
