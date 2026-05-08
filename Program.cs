a6a33b1bfd8ae82079d121552ccda4fa3ed7b90e
idesignerSe <hebera75@gmail.com>
2026-05-08 11:31:49 +0200

    List of Products and Sorting them out

--- Program.cs
+++ Program.cs
@@ -6,6 +6,37 @@ class Program
     {
         Console.WriteLine("PRODUCT LIST MANAGER");
         Console.WriteLine("Press any key to exit...");
+        Console.WriteLine("Type 'exit' to finish. \n ");
+
+        List<string> products = new List<string>();
+
+        while (true)
+        {
+            while (true)
+        {
+            Console.Write("Product: ");
+            string input = Console.ReadLine();
+
+            if (input == null)
+                continue;
+
+            string normalized = input.Trim().ToLower();
+
+            if (normalized == "exit")
+                break;
+
+            products.Add(input.Trim());
+        }
+
+        products.Sort();
+        Console.WriteLine("\n Sorted Product List:\n");
+
+        foreach (var p in products)
+        {
+            Console.WriteLine("- " + p);
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
         Console.ReadKey();
     }
 }
